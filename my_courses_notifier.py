e1b21c9ac62a0878f88b358e18c0cacd06de32be
Gal Amrani <galamrani2@gmail.com>
2023-08-06 17:57:06 +0300

    Add comments to the source code

--- my_courses_notifier.py
+++ my_courses_notifier.py
@@ -6,10 +6,9 @@ from winotify import Notification, audio
 
 # Constants
 COURSE_ID = "20554"     # Replace with your desired course ID
-YEAR = "2024"           # Replace with the desired semester (A OR B OR C)
+YEAR = "2024"           # Replace with the desired year
 SEMESTER  = "A"         # Replace with the desired semester (A OR B OR C)
 DRIVER_PATH  = "C:\Program Files\chromedriver.exe"   # Replace with the path to your Chrome WebDriver 
-
 CSV_FILE = "course_identifiers.csv"
 SEMESTER_DICT = {
     "A" : "05D0",
@@ -19,17 +18,20 @@ SEMESTER_DICT = {
 WEBSITE = "https://sheilta.apps.openu.ac.il/CoursesRegistration/Pages/GroupSearchResults.aspx?Semester=" + YEAR + "%u" + SEMESTER_DICT[SEMESTER]+ "&CourseIDs=," + COURSE_ID  + ",&Degree=0&GeographicalArea=,01,02,03,04,05,06,07,09,&InstructionHours=00:01&Days=111111&Page=1"
 
 
+# Function to scrape course IDs and study group elements from the website
 def scrape_course_ids():
     try:
         courses_ids = []
         driver = webdriver.Chrome(DRIVER_PATH)
         driver.get(WEBSITE)
 
+        # Find elements containing course codes and study group information
         main_table = driver.find_element_by_id("main_table")
         course_code_elements = main_table.find_elements_by_xpath("//tr/td[5]")
         study_group_elements = main_table.find_elements_by_xpath("//tr/td[7]")
         course_code_elements.pop(-1)
 
+        # Collect course IDs and study group elements into a list
         for i in range(len(course_code_elements )):
             course_id = (course_code_elements[i].text, study_group_elements[i].text)
             courses_ids.append(course_id)
@@ -43,6 +45,7 @@ def scrape_course_ids():
         return []
 
 
+# Function to check for new courses and notify the user if found
 def check_and_notify_new_courses(existing_ids):
 
     if os.path.exists(CSV_FILE):
@@ -54,21 +57,25 @@ def check_and_notify_new_courses(existing_ids):
             except StopIteration:
                 csv_data = []  
             
+            # Compare existing course IDs with those in the CSV file          
             for course_id  in existing_ids:
                 course_id_str = str(course_id)
                 if course_id_str not in csv_data:
                     courses_ids.append(course_id_str)
-
+            
+            # Notify the user about new courses
             if len(courses_ids) > 0:
                 notify_new_course(courses_ids)
 
 
+# Function to save course IDs to a CSV file
 def save_course_ids_to_csv(ids_list):
     with open(CSV_FILE, "w") as csv_file:
         csv_writer = csv.writer(csv_file)
         csv_writer.writerow(ids_list)
 
 
+# Function to display a notification about new courses
 def notify_new_course(courses_ids):
     separator = ','
     result_string = separator.join(courses_ids)
@@ -83,9 +90,9 @@ def notify_new_course(courses_ids):
 
 
 if __name__ == "__main__":
-    courses_ids = scrape_course_ids()
-    check_and_notify_new_courses(courses_ids)
-    save_course_ids_to_csv(courses_ids)
+    courses_ids = scrape_course_ids()   # Scrape course IDs from the website
+    check_and_notify_new_courses(courses_ids)   # Check for new courses and notify if found
+    save_course_ids_to_csv(courses_ids) # Save the current course IDs to a CSV file for future comparison
 
 
 
